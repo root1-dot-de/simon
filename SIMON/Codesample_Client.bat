55602b5c0476fd75b2d427df2946c821062702d3
Alexander Christian <alex@root1.de>
2008-03-11 15:30:11 +0000

    initial commit

---
+++ SIMON/Codesample_Client.bat
@@ -0,0 +1,4 @@
+REM
+REM Verbinde dich mit Server an 127.0.0.1
+REM
+java -cp bin_test_client;bin_test_common;bin de.root1.simon.codesample.client.SampleClient 127.0.0.1
---
+++ SIMON/src/de/root1/simon/SimonRemoteException.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2008 Alexander Christian <alex(at)root1.de>. All rights reserved.
+ * 
+ * This file is part of SIMON.
+ *
+ *   SIMON is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SIMON is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SIMON.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.root1.simon;
+
+
+public class SimonRemoteException extends RuntimeException{
+	
+	/**
+	 * TODO: Documentation to be done for field 'serialVersionUID', by 'ACHR'..
+	 *
+	 */
+	private static final long serialVersionUID = -4586636119330576150L;
+
+	public SimonRemoteException(String msg) {
+		super(msg);
+	}
+
+	public SimonRemoteException(Exception e) {
+		super(e);
+	}
+
+}
