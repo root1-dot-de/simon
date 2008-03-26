d14b921206bc2bb7dcfbf5e6d6b9845b2e9883ca
alex0801 <alex0801@8a166049-a23f-4faa-ba0f-b48904010694>
2008-03-26 19:38:49 +0000

    imports organized

--- branches/simon_nio/SIMON/src/de/root1/simon/nio/NioClient.java
+++ branches/simon_nio/SIMON/src/de/root1/simon/nio/NioClient.java
@@ -2,13 +2,18 @@ package de.root1.simon.nio;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.nio.channels.spi.SelectorProvider;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class NioClient implements Runnable {
 	// The host:port combination to connect to
--- branches/simon_nio/SIMON/src/de/root1/simon/nio/NioServer.java
+++ branches/simon_nio/SIMON/src/de/root1/simon/nio/NioServer.java
@@ -9,7 +9,12 @@ import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.channels.spi.SelectorProvider;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class NioServer implements Runnable {
 	// The host:port combination to listen on
