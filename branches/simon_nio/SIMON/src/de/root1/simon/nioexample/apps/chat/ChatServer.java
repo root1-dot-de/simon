9f714f920485c1e69af13aabd0bc3c6e6e4b905d
alex0801 <alex0801@8a166049-a23f-4faa-ba0f-b48904010694>
2008-04-03 19:48:25 +0000

    renamed

---
+++ branches/simon_nio/SIMON/src/de/root1/simon/nioexample/apps/chat/ChatServer.java
@@ -0,0 +1,32 @@
+package de.root1.simon.nioexample.apps.chat;
+
+import java.io.IOException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import de.root1.simon.nioexample.impl.Acceptor;
+import de.root1.simon.nioexample.impl.Dispatcher;
+import de.root1.simon.nioexample.impl.DumbBufferFactory;
+import de.root1.simon.nioexample.impl.IInputHandlerFactory;
+import de.root1.simon.nioexample.interfaces.IByteBufferFactory;
+
+public class ChatServer
+{
+	private ChatServer()
+	{
+		// cannot instantiate
+	}
+
+	public static void main (String [] args)
+		throws IOException
+	{
+		Executor executor = Executors.newCachedThreadPool();
+		IByteBufferFactory bufFactory = new DumbBufferFactory (1024);
+		Dispatcher dispatcher = new Dispatcher (executor, bufFactory);
+		IInputHandlerFactory factory = new ChatProtocol();
+		Acceptor acceptor = new Acceptor (1234, dispatcher, factory);
+
+		dispatcher.start();
+		acceptor.newThread();
+	}
+}
