2012dd23c383228a7f3798cc57846ae6a1d66349
alex0801 <alex0801@8a166049-a23f-4faa-ba0f-b48904010694>
2008-04-09 15:54:08 +0000

    not needed anymore

--- branches/simon_nio/SIMON/src/de/root1/simon/nioexample/interfaces/IChannelFacade.java
+++
@@ -1,9 +0,0 @@
-package de.root1.simon.nioexample.interfaces;
-
-public interface IChannelFacade {
-	IInputQueue inputQueue();
-	IOutputQueue outputQueue();
-	void setHandler(IInputHandler handler);
-	int getInterestOps();
-	void modifyInterestOps(int opsToSet, int opsToReset);
-}
