253b72fe253821b0717438a400751db0bb5e2646
Alex <2862906+tuxedo0801@users.noreply.github.com>
2008-07-24 17:47:15 +0000

    sample implementation of unreferenced()

--- SIMON/simon_testserver/src/ServerSessionImpl.java
+++ SIMON/simon_testserver/src/ServerSessionImpl.java
@@ -1,11 +1,17 @@
+import de.root1.simon.SimonUnreferenced;
 import de.root1.simon.exceptions.SimonRemoteException;
 
-public class ServerSessionImpl implements ServerSessionInterface {
+public class ServerSessionImpl implements ServerSessionInterface, SimonUnreferenced {
 
 	private static final long serialVersionUID = 1L;
 
 	public void printMessageOnServer(String msg) throws SimonRemoteException {
 		System.out.println("Message from Client: "+msg);
+	}
+
+	@Override
+	public void unreferenced() {
+		System.out.println("I got unreferenced!");
 	}
 
 }
