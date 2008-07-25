8f8264807f46c00007f895ec328e3245a92018c2
Alexander Christian <alex@root1.de>
2008-07-25 14:26:19 +0000

    files not needed anymore

--- SIMON/simon/src/de/root1/simon/tests/references/MyObject.java
+++
@@ -1,9 +0,0 @@
-package de.root1.simon.tests.references;
-class MyObject
-{
-  protected void finalize() throws Throwable
-  {
-    System.err.println("In finalize method for this object: " + this);
-  }
-}
-
