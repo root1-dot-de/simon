51c803fbfbd393a4e2f231481c900ac21751f558
alex0801 <alex0801@8a166049-a23f-4faa-ba0f-b48904010694>
2008-09-03 10:10:43 +0000

    fix for issue #22

--- trunk/SIMON/simon/src/de/root1/simon/tests/server/SampleServer.java
+++ trunk/SIMON/simon/src/de/root1/simon/tests/server/SampleServer.java
@@ -4,17 +4,19 @@ package de.root1.simon.tests.server;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
+import de.root1.simon.Registry;
 import de.root1.simon.Simon;
+import de.root1.simon.exceptions.NameBindingException;
 
 public class SampleServer {
 	
-	public static void main(String[] args) throws InterruptedException, IllegalStateException, IOException {
+	public static void main(String[] args) throws InterruptedException, IllegalStateException, IOException, NameBindingException {
 			
 //		Utils.DEBUG = true;
 		
 		ServerInterfaceImpl serverImpl = new ServerInterfaceImpl();
-		Simon.createRegistry(2000);
-		Simon.bind("server", serverImpl);
+		Registry serverRegistry = Simon.createRegistry(2000);
+		serverRegistry.bind("server", serverImpl);
 		System.out.println("Server gestartet");
 				
 	}
