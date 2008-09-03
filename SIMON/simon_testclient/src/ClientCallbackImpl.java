835a275bac4c92a17d45d2d089d3e7e2be9a1f76
Alexander Christian <alex@root1.de>
2008-09-03 10:12:34 +0000

    changed to stay compatible with simon

--- SIMON/simon_testclient/src/ClientCallbackImpl.java
+++ SIMON/simon_testclient/src/ClientCallbackImpl.java
@@ -7,7 +7,7 @@ public class ClientCallbackImpl implements ClientCallbackInterface {
 	private static final long serialVersionUID = 1L;
 
 	public void callback(String text) throws SimonRemoteException {
-		System.out.println("Die folgende Meldung stammt vom Server: "+text);
+		System.out.println("ClientCallbackInterface: Die folgende Meldung stammt vom Server: "+text);
 	}
 
 }
