09a10a8ae9b226c0cbfff51543f8864530739606
alex0801 <alex0801@8a166049-a23f-4faa-ba0f-b48904010694>
2008-09-03 10:57:07 +0000

    changed to stay compatible with simon

--- trunk/SIMON/simon_testshared/src/ServerSessionInterface.java
+++ trunk/SIMON/simon_testshared/src/ServerSessionInterface.java
@@ -1,9 +1,10 @@
 
 
 import de.root1.simon.SimonRemote;
+import de.root1.simon.SimonUnreferenced;
 import de.root1.simon.exceptions.SimonRemoteException;
 
-public interface ServerSessionInterface extends SimonRemote {
+public interface ServerSessionInterface extends SimonRemote, SimonUnreferenced {
 	
 	public void printMessageOnServer(String msg) throws SimonRemoteException;
 
