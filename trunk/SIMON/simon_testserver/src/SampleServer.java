68a33209a73bf2e751dc30da7c8e282c2bb08de8
alex0801 <alex0801@8a166049-a23f-4faa-ba0f-b48904010694>
2008-09-03 11:05:21 +0000

    changed to stay compatible with simon

--- trunk/SIMON/simon_testserver/src/SampleServer.java
+++ trunk/SIMON/simon_testserver/src/SampleServer.java
@@ -1,21 +1,32 @@
 
 
-import java.net.UnknownHostException;
+import java.io.IOException;
+import java.net.InetAddress;
 
+import de.root1.simon.Registry;
 import de.root1.simon.Simon;
+import de.root1.simon.exceptions.NameBindingException;
 import de.root1.simon.utils.Utils;
 
 public class SampleServer {
 	
-	public static void main(String[] args) throws InterruptedException, UnknownHostException, IllegalStateException {
-			
+	public static void main(String[] args) throws InterruptedException, IllegalStateException, IOException, NameBindingException {
+
+		// for enabling debug mode
 		Utils.DEBUG = true;
-		
+
+		// create the server implementation
 		ServerInterfaceImpl serverImpl = new ServerInterfaceImpl();
-		Simon.createRegistry(2000);
-		Simon.bind("server", serverImpl);
-		System.out.println("Server gestartet");
 		
+		// create the registry where we bind the server implementation
+		Registry serverRegistry = Simon.createRegistry(InetAddress.getByName("127.0.0.1"), 2000);
+		
+		// and finally find the implementation
+		serverRegistry.bind("server", serverImpl);
+		
+		// to stop the server, call stop() on the registry
+		//serverRegistry.stop();
+				
 	}
 
 }
--- trunk/SIMON/simon_testserver/src/ServerSessionImpl.java
+++ trunk/SIMON/simon_testserver/src/ServerSessionImpl.java
@@ -4,13 +4,19 @@ import de.root1.simon.exceptions.SimonRemoteException;
 public class ServerSessionImpl implements ServerSessionInterface, SimonUnreferenced {
 
 	private static final long serialVersionUID = 1L;
+	private Object o;
+	
+	public ServerSessionImpl(Object o) {
+		this.o = o;
+	}
 
 	public void printMessageOnServer(String msg) throws SimonRemoteException {
 		System.out.println("Message from Client: "+msg);
 	}
 
+	@Override
 	public void unreferenced() {
-		System.out.println("I got unreferenced!");
+		System.out.println("I got unreferenced! -> "+o);
 	}
 
 }
