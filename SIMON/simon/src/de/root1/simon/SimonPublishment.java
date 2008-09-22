6854e6217977bbf5cd558077cceec75768764a71
Alexander Christian <alex@root1.de>
2008-09-22 12:05:41 +0000

    finished implementation of "SimonPublishement". Added all java doc informations.

--- SIMON/simon/src/de/root1/simon/SimonPublishment.java
+++ SIMON/simon/src/de/root1/simon/SimonPublishment.java
@@ -19,10 +19,18 @@
 package de.root1.simon;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
- * TODO document me
- * @author ACHR
+ * This class holds a 3-tupel of data needed to identify a remote object on a registry.
+ * The tupel constsits of:
+ * <ol>
+ * <li>address to which the registry of the remote object is bind to</li>
+ * <li>the port on which the registry listens</li>
+ * <li>the remote object name bind to the registry</li>
+ * </ol>
+ * 
+ * @author achristian
  *
  */
 public class SimonPublishment {
@@ -33,25 +41,67 @@ public class SimonPublishment {
 	private InetAddress address;
 	StringBuffer sb = new StringBuffer();
 
+	/**
+	 * Creates a new object instance according to the given address, port and remote object name
+	 * @param address the address of a simon registry
+	 * @param port the port on which the registry listens
+	 * @param remoteObjectName the bind remote object name
+	 */
 	public SimonPublishment(InetAddress address, int port, String remoteObjectName) {
 		this.address = address;
 		this.port = port;
 		this.remoteObjectName = remoteObjectName;
 	}
 	
-	public SimonPublishment(String rawString) throws IllegalArgumentException {
+	/**
+	 * Creates an object instance according to a raw string with the format:<br>
+	 * <i>[SIMON|192.168.0.123:1234|myServer]</i>
+	 * 
+	 * @param rawString the string that delivers the needed data to create an instance of {@link SimonPublishment} 
+	 * @throws IllegalArgumentException if raw string has the wrong format
+	 * @throws UnknownHostException if the host in the raw string is unknown
+	 * @throws NumberFormatException if the port in the raw string has the wrong format (e.g. non numeric)
+	 */
+	public SimonPublishment(String rawString) throws IllegalArgumentException, UnknownHostException, NumberFormatException {
 		if (!rawString.substring(0, SIMON_PUBLISHMENT_HEADER.length()).equals(SIMON_PUBLISHMENT_HEADER) ) 
 			throw new IllegalArgumentException("provided raw string has the wrong format: "+rawString);
+		
+		String values = rawString.substring(SIMON_PUBLISHMENT_HEADER.length(), rawString.length()-1);
+		
+		String[] valuesSplit = values.split("\\|");
+		if (valuesSplit.length!=2) throw new IllegalArgumentException("except the header, there must be two values in the raw string. 1st: 'ip:port' 2nd: 'remoteObjectName'");
+		
+		String[] addressSplit = valuesSplit[0].split(":");
+		
+		if (addressSplit.length!=2) throw new IllegalArgumentException("the address value must contain two values: 1st: 'ip' 2nd: 'port'.");
+		
+		address = InetAddress.getByName(addressSplit[0]);
+		port = Integer.parseInt(addressSplit[1]);
+		
+		if (valuesSplit[1]==null || valuesSplit[1].length()<1) throw new IllegalArgumentException("'remoteObjectName' must not be 'null' or zero-length.");
+		remoteObjectName = valuesSplit[1];
 	}
 	
+	/**
+	 * Returns the name of the remote object which is bind to the registry
+	 * @return name of the remote object
+	 */
 	public String getRemoteObjectName() {
 		return remoteObjectName;
 	}
 	
+	/**
+	 * Returns the port on which the registry listens for connections
+	 * @return port on which the registry listens
+	 */
 	public int getPort() {
 		return port;
 	}
 	
+	/**
+	 * Returns the network address to which the registry is bind
+	 * @return an address of the registry
+	 */
 	public InetAddress getAddress() {
 		return address;
 	}
