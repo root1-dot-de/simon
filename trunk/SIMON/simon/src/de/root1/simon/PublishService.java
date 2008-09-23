43a573bdc59993ce357c07edef4438837f8cd6d2
alex0801 <alex0801@8a166049-a23f-4faa-ba0f-b48904010694>
2008-09-23 16:14:56 +0000

    added a block searchForRemoteObjects method added more javadoc

--- trunk/SIMON/simon/src/de/root1/simon/PublishService.java
+++ trunk/SIMON/simon/src/de/root1/simon/PublishService.java
@@ -23,7 +23,6 @@ import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.SocketTimeoutException;
-import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
 
