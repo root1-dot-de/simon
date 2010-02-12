f287969be2352754cc3d1dc3813f7bff102b1236
achristian <achristian@8a166049-a23f-4faa-ba0f-b48904010694>
2010-02-12 16:16:52 +0000

    ManualTestAnnotation.java is able to use new lookup mechanism. Release stuff is still "missing".

--- trunk/src/test/java/de/root1/simon/test/annotation/ManualTestAnnotation.java
+++ trunk/src/test/java/de/root1/simon/test/annotation/ManualTestAnnotation.java
@@ -5,6 +5,7 @@
 
 package de.root1.simon.test.annotation;
 
+import de.root1.simon.Lookup;
 import de.root1.simon.Registry;
 import de.root1.simon.Simon;
 import java.io.File;
@@ -21,41 +22,40 @@ public class ManualTestAnnotation {
 
     public static void main(String[] args) {
 
-        File f = new File("target/test-classes/simon_logging.properties");
-        try {
-            FileInputStream is = new FileInputStream(f);
-            LogManager.getLogManager().readConfiguration(is);
-            System.out.println("logger initiated");
-
-        } catch (FileNotFoundException e) {
-
-                System.err.println("File not found: "+f.getAbsolutePath()+".\n" +
-                                "If you don't want to debug SIMON, leave 'Utils.DEBUG' with false-value.\n" +
-                                "Otherwise you have to provide a Java Logging API conform properties-file like mentioned.");
-
-        } catch (SecurityException e) {
-
-                System.err.println("Security exception occured while trying to load "+f.getAbsolutePath()+"\n" +
-                                "Logging with SIMON not possible!.");
-
-        } catch (IOException e) {
-
-                System.err.println("Cannot load "+f.getAbsolutePath()+" ...\n" +
-                                "Please make sure that Java has access to that file.");
-
-        }
+//        File f = new File("target/test-classes/simon_logging.properties");
+//        try {
+//            FileInputStream is = new FileInputStream(f);
+//            LogManager.getLogManager().readConfiguration(is);
+//            System.out.println("logger initiated");
+//
+//        } catch (FileNotFoundException e) {
+//
+//                System.err.println("File not found: "+f.getAbsolutePath()+".\n" +
+//                                "If you don't want to debug SIMON, leave 'Utils.DEBUG' with false-value.\n" +
+//                                "Otherwise you have to provide a Java Logging API conform properties-file like mentioned.");
+//
+//        } catch (SecurityException e) {
+//
+//                System.err.println("Security exception occured while trying to load "+f.getAbsolutePath()+"\n" +
+//                                "Logging with SIMON not possible!.");
+//
+//        } catch (IOException e) {
+//
+//                System.err.println("Cannot load "+f.getAbsolutePath()+" ...\n" +
+//                                "Please make sure that Java has access to that file.");
+//
+//        }
 
         try {
             RemoteObjectImpl roi = new RemoteObjectImpl();
 
             Registry r = Simon.createRegistry(22222);
             r.bind("roi", roi);
+            Lookup lookup = Simon.createLookup("localhost", 22222);
 
-            RemoteObject roiRemote = null;
-            // TODO FIXME
-//            (RemoteObject) Simon.lookup("localhost", 22222, "roi");
+            RemoteObject roiRemote = (RemoteObject) lookup.lookup("roi");
 
-            System.out.println("got remote object");
+            System.out.println("got remote object: "+roiRemote);
 
 //            System.out.println("equals: "+roiRemote.equals(roiRemote));
 //
