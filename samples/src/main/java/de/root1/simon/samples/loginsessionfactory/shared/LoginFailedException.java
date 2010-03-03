45563a63e2141bc43837270b817559ea2f881505
Alexander Christian <alex@root1.de>
2010-03-03 16:39:23 +0000

    added login session factory sample.

---
+++ samples/src/main/java/de/root1/simon/samples/loginsessionfactory/shared/LoginFailedException.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.root1.simon.samples.loginsessionfactory.shared;
+
+
+/**
+ *
+ * @author ACHR
+ */
+public class LoginFailedException extends Exception {
+
+    public LoginFailedException(String msg) {
+        super(msg);
+    }
+
+}
