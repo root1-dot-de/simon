a61b7dc60f9089d3ab6802cc1841a519dd2b81c0
Alex <2862906+tuxedo0801@users.noreply.github.com>
2010-04-09 15:11:51 +0000

    fixed JUnit Test for annotation testing

--- src/test/java/de/root1/simon/test/annotation/TestAnnotation.java
+++ src/test/java/de/root1/simon/test/annotation/TestAnnotation.java
@@ -43,7 +43,56 @@ public class TestAnnotation {
     }
 
     @Test
-    public void testEqualsNull() {
+    public void testExportedInterface() {
+
+        File f = new File("target/test-classes/simon_logging.properties");
+        try {
+            FileInputStream is = new FileInputStream(f);
+            LogManager.getLogManager().readConfiguration(is);
+
+
+        } catch (FileNotFoundException e) {
+
+                System.err.println("File not found: "+f.getAbsolutePath()+".\n" +
+                                "If you don't want to debug SIMON, leave 'Utils.DEBUG' with false-value.\n" +
+                                "Otherwise you have to provide a Java Logging API conform properties-file like mentioned.");
+
+        } catch (SecurityException e) {
+
+                System.err.println("Security exception occured while trying to load "+f.getAbsolutePath()+"\n" +
+                                "Logging with SIMON not possible!.");
+
+        } catch (IOException e) {
+
+                System.err.println("Cannot load "+f.getAbsolutePath()+" ...\n" +
+                                "Please make sure that Java has access to that file.");
+
+        }
+        try {
+            RemoteObjectImpl roi = new RemoteObjectImpl();
+
+            Registry r = Simon.createRegistry(22222);
+            r.bind("roi", roi);
+            Lookup lookup = Simon.createNameLookup("localhost", 22222);
+
+            RemoteObject1 roiRemote = (RemoteObject1) lookup.lookup("roi");
+
+            roiRemote.myRemoteMethod1();
+
+            lookup.release(roiRemote);
+
+            r.unbind("roi");
+            r.stop();
+        
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new AssertionError("It should be able to access an exported interface. ExceptionMsg: "+ex.getMessage());
+        }
+
+    }
+
+    @Test
+    public void testUnexportedInterface() {
 
         File f = new File("target/test-classes/simon_logging.properties");
         try {
@@ -76,18 +125,19 @@ public class TestAnnotation {
             r.bind("roi", roi);
             Lookup lookup = Simon.createNameLookup("localhost", 22222);
 
-            RemoteObject1 roiRemote = (RemoteObject1) lookup.lookup("roi");
+            RemoteObject3 roiRemote = (RemoteObject3) lookup.lookup("roi");
+            if (roiRemote!=null)
+                throw new AssertionError("It should not be possible to access a interface that is not exported.");
 
-            roiRemote.equals(null);
-            roiRemote.myRemoteMethod1();
+            roiRemote.myRemoteMethod3();
 
             lookup.release(roiRemote);
 
             r.unbind("roi");
             r.stop();
-        
+
         } catch (Exception ex) {
-            new AssertionError("Error during test: "+ex.getMessage());
+            System.out.println("Success! Can't call myMethod3(). ExceptionMsg: "+ex.getMessage());
         }
 
     }
