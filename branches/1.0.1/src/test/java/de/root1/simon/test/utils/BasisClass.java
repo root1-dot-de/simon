baea3112680a5014fec52927be7503da9077c443
achristian <achristian@8a166049-a23f-4faa-ba0f-b48904010694>
2011-01-11 14:03:10 +0000

    not needed

--- branches/1.0.1/src/test/java/de/root1/simon/test/utils/BasisClass.java
+++
@@ -1,5 +0,0 @@
-package de.root1.simon.test.utils;
-
-public class BasisClass implements ExtendedBasisInterface {
-	private static final long serialVersionUID = 7059327535633065297L;
-}
--- branches/1.0.1/src/test/java/de/root1/simon/test/utils/BasisInterface.java
+++
@@ -1,6 +0,0 @@
-package de.root1.simon.test.utils;
-
-import de.root1.simon.SimonRemote;
-
-public interface BasisInterface extends SimonRemote {
-}
