38a247432de769cdfa208eb1f824ad1ecd58845f
Alexander Christian <alex@root1.de>
2011-03-23 15:43:18 +0000

    first step to fix ChangeRequest #92

---
+++ src/test/java/de/root1/simon/test/utils/ExtendedInterfaceWithRemoteannotation.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.root1.simon.test.utils;
+
+import de.root1.simon.annotation.SimonRemote;
+
+/**
+ *
+ * @author achr
+ */
+@SimonRemote
+public interface ExtendedInterfaceWithRemoteannotation extends InterfaceWithRemoteannotation {
+       
+}
