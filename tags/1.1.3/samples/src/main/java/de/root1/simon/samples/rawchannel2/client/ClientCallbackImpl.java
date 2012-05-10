11f4bb37af8c090de076275dddfd4aeab940a8ca
achristian <achristian@8a166049-a23f-4faa-ba0f-b48904010694>
2012-05-10 07:48:30 +0000

    tagged 1.1.x branch as 1.1.3

---
+++ tags/1.1.3/samples/src/main/java/de/root1/simon/samples/rawchannel2/client/ClientCallbackImpl.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.root1.simon.samples.rawchannel2.client;
+
+import de.root1.simon.Simon;
+import de.root1.simon.exceptions.SimonRemoteException;
+import de.root1.simon.samples.rawchannel2.shared.ClientCallback;
+
+/**
+ *
+ * @author ACHR
+ */
+public class ClientCallbackImpl implements ClientCallback {
+
+    // register the file receiver and return the channel token
+    public int prepareFileTransfer(final String fileName) throws SimonRemoteException {
+        return Simon.prepareRawChannel(new FileReceiver(fileName), this);
+    }
+
+}
---
+++ tags/1.1.3/src/test/java/de/root1/simon/test/markAsRemote/IPojo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.root1.simon.test.markAsRemote;
+
+import de.root1.simon.annotation.SimonRemote;
+
+/**
+ *
+ * @author achristian
+ */
+@SimonRemote
+public interface IPojo {
+
+    String getHelloName(String name);
+
+    void printHelloName(String name);
+
+}
