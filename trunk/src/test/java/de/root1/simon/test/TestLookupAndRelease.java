42c6e0ae5e14e8b21207f5c5fff63fd7d439573d
achristian <achristian@8a166049-a23f-4faa-ba0f-b48904010694>
2012-06-08 10:30:04 +0000

    Added LookupAndReleaseTwiceWithoutSleep JUnit Test

--- trunk/src/test/java/de/root1/simon/test/TestLookupAndRelease.java
+++ trunk/src/test/java/de/root1/simon/test/TestLookupAndRelease.java
@@ -105,5 +105,66 @@ public class TestLookupAndRelease {
             throw new AssertionError(ex);
         }
     }
+    
+    @Test
+    public void LookupAndReleaseTwiceWithoutSleep() {
+
+        try {
+
+            logger.info("Begin ...");
+            RemoteObjectImpl roi = new RemoteObjectImpl();
+
+            Registry r = Simon.createRegistry(33333);
+            r.bind("roi", roi);
+
+            logger.info("bound roi to registry ...");
+            Lookup lookup = Simon.createNameLookup("localhost", 33333);
+
+            RemoteObject roiRemote = (RemoteObject) lookup.lookup("roi");
+
+            logger.info("roi lookup done");
+
+            roiRemote.helloWorld();
+            logger.info("method call done");
+
+            lookup.release(roiRemote);
+            logger.info("release of roi done");
+            
+//            logger.info("Awaiting network connections shutdown");
+//            ((NameLookup) lookup).awaitCompleteShutdown(30000);
+//            logger.info("Awaiting network connections shutdown *done*");
+
+            r.unbind("roi");
+            logger.info("unbind of roi done");
+            r.stop();
+            logger.info("registry stopped");
+
+            // ----------------
+            // Wait a few sec to give the OS soe time to really release the socket
+//            Thread.sleep(5000);
+
+            RemoteObjectImpl roi2 = new RemoteObjectImpl();
+            Registry r2 = Simon.createRegistry(33333);
+            r2.bind("roi2", roi2);
+            Lookup lookup2 = Simon.createNameLookup("localhost", 33333);
+
+            RemoteObject roiRemote2 = (RemoteObject) lookup.lookup("roi2");
+            roiRemote2.helloWorld();
+            lookup2.release(roiRemote2);
+            
+//            logger.info("Awaiting network connections shutdown");
+//            ((NameLookup) lookup).awaitCompleteShutdown(30000);
+//            logger.info("Awaiting network connections shutdown *done*");
+            
+
+            r2.unbind("roi2");
+            r2.stop();
+
+            assert true;
+
+        } catch (Exception ex) {
+            throw new AssertionError(ex);
+        }
+    }
 
 }
