5fd6399332f26e9e2aadcf0e9fc63634c9d3fdcf
Alexander Christian <alex@root1.de>
2012-06-21 19:58:11 +0000

    some simple (non junit) tests

---
+++ src/test/java/de/root1/simon/test/custominvoketimeout/B.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.root1.simon.test.custominvoketimeout;
+
+/**
+ *
+ * @author achristian
+ */
+public class B {
+    
+    public void helloWorld() {
+        
+    }
+    
+}
