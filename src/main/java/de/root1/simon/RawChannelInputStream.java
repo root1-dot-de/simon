ae5a978fe780364059cd01b9b7919582dd20c1d7
Alexander Christian <alex@root1.de>
2013-04-09 13:31:00 +0000

    #85 Added missing GPL info

--- src/main/java/de/root1/simon/RawChannelInputStream.java
+++ src/main/java/de/root1/simon/RawChannelInputStream.java
@@ -1,6 +1,20 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2013 Alexander Christian <alex(at)root1.de>. All rights reserved.
+ * 
+ * This file is part of SIMON.
+ *
+ *   SIMON is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SIMON is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SIMON.  If not, see <http://www.gnu.org/licenses/>.
  */
 package de.root1.simon;
 
@@ -11,17 +25,18 @@ import java.io.PipedOutputStream;
 import java.nio.ByteBuffer;
 
 /**
+ * InputStream wrapper for SIMON RawChannel
  *
  * @author achristian
+ * @since 1.2.0
  */
 public class RawChannelInputStream extends InputStream {
 
     private RawChannelDataListener dataListener = new RawChannelDataListener() {
-
         @Override
         public void write(ByteBuffer data) {
             int len = data.limit();
-            data.get(buf,0,len);
+            data.get(buf, 0, len);
             try {
                 pos.write(buf, 0, len);
             } catch (IOException ex) {
@@ -38,30 +53,42 @@ public class RawChannelInputStream extends InputStream {
             }
         }
     };
-    
     private IOException exception;
     private PipedInputStream pis;
     private PipedOutputStream pos;
     private final byte[] buf;
-    
     /**
      * Default block size of 8k
      */
     public static final int DEFAULT_BLOCKSIZE = 8 * 1024;
 
+    /**
+     * Creates a new input stream. You need to connect it to SIMON via
+     * {@link Simon#prepareRawChannel(de.root1.simon.RawChannelDataListener, java.lang.Object)}.
+     * Pass the output of
+     * {@link RawChannelInputStream#getRawChannelDataListener()} as
+     * {@link RawChannelDataListener}.
+     *
+     * @throws IOException
+     */
     public RawChannelInputStream() throws IOException {
         this(DEFAULT_BLOCKSIZE);
     }
 
+    /**
+     * @see RawChannelInputStream#RawChannelInputStream() 
+     * @param blockSize the block-size for reading from rawchannel and wriuting to the inputstream pipe
+     * @throws IOException 
+     */
     public RawChannelInputStream(int blockSize) throws IOException {
         buf = new byte[blockSize];
         pos = new PipedOutputStream();
         pis = new PipedInputStream(pos, blockSize);
     }
-    
+
     @Override
     public int read() throws IOException {
-        if (exception!=null) {
+        if (exception != null) {
             throw new IOException("Error occured while forwarding data from RawChannelDataListener to InputStream", exception);
         }
         return pis.read();
@@ -69,7 +96,7 @@ public class RawChannelInputStream extends InputStream {
 
     @Override
     public int read(byte[] b) throws IOException {
-        if (exception!=null) {
+        if (exception != null) {
             throw new IOException("Error occured while forwarding data from RawChannelDataListener to InputStream", exception);
         }
         return pis.read(b);
@@ -77,7 +104,7 @@ public class RawChannelInputStream extends InputStream {
 
     @Override
     public int read(byte[] b, int off, int len) throws IOException {
-        if (exception!=null) {
+        if (exception != null) {
             throw new IOException("Error occured while forwarding data from RawChannelDataListener to InputStream", exception);
         }
         return pis.read(b, off, len);
@@ -85,7 +112,7 @@ public class RawChannelInputStream extends InputStream {
 
     @Override
     public synchronized void reset() throws IOException {
-        if (exception!=null) {
+        if (exception != null) {
             throw new IOException("Error occured while forwarding data from RawChannelDataListener to InputStream", exception);
         }
         pis.reset();
@@ -93,7 +120,7 @@ public class RawChannelInputStream extends InputStream {
 
     @Override
     public int available() throws IOException {
-        if (exception!=null) {
+        if (exception != null) {
             throw new IOException("Error occured while forwarding data from RawChannelDataListener to InputStream", exception);
         }
         return pis.available();
@@ -101,7 +128,7 @@ public class RawChannelInputStream extends InputStream {
 
     @Override
     public void close() throws IOException {
-        if (exception!=null) {
+        if (exception != null) {
             throw new IOException("Error occured while forwarding data from RawChannelDataListener to InputStream", exception);
         }
         pos.close();
@@ -120,19 +147,19 @@ public class RawChannelInputStream extends InputStream {
 
     @Override
     public long skip(long n) throws IOException {
-        if (exception!=null) {
+        if (exception != null) {
             throw new IOException("Error occured while forwarding data from RawChannelDataListener to InputStream", exception);
         }
         return pis.skip(n);
     }
 
     /**
-     * Get the underlying SIMON RawChannelDataListener implementation for this input stream.
-     * 
+     * Get the underlying SIMON RawChannelDataListener implementation for this
+     * input stream.
+     *
      * @return RawChannelDataListener implementation
      */
     public RawChannelDataListener getRawChannelDataListener() {
         return dataListener;
     }
-    
 }
