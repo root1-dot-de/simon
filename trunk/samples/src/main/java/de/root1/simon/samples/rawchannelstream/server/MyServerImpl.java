e49982fb2c2913129abede2629a878f30a30c4da
achristian <achristian@8a166049-a23f-4faa-ba0f-b48904010694>
2013-04-09 14:34:58 +0000

    #85 update sample

--- trunk/samples/src/main/java/de/root1/simon/samples/rawchannelstream/server/MyServerImpl.java
+++ trunk/samples/src/main/java/de/root1/simon/samples/rawchannelstream/server/MyServerImpl.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.root1.simon.samples.rawchannelstream.server;
 
 import de.root1.simon.RawChannel;
@@ -10,25 +6,10 @@ import de.root1.simon.Simon;
 import de.root1.simon.annotation.SimonRemote;
 import de.root1.simon.samples.rawchannelstream.shared.ClientCallback;
 import de.root1.simon.samples.rawchannelstream.shared.MyServer;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-/**
- *
- * @author ACHR
- */
 @SimonRemote(value = {MyServer.class})
 public class MyServerImpl implements MyServer {
-
-    @Override
-    public void doSomething() {
-        System.out.println("Doing something on server ...");
-    }
 
     @Override
     public void requestStream(ClientCallback clientCallback) {
