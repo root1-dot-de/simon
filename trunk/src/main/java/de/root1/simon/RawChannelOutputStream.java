a3df4d23bc0f6a66481fa27a47463dbfc49aca45
achristian <achristian@8a166049-a23f-4faa-ba0f-b48904010694>
2013-04-09 13:31:00 +0000

    #85 Added missing GPL info

--- trunk/src/main/java/de/root1/simon/RawChannelOutputStream.java
+++ trunk/src/main/java/de/root1/simon/RawChannelOutputStream.java
@@ -1,6 +1,20 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2013 Alexander Christian <alex(at)root1.de>. All rights reserved.
+ * 
+ * This file is part of SIMON.
+ *
+ *   SIMON is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SIMON is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SIMON.  If not, see <http://www.gnu.org/licenses/>.
  */
 package de.root1.simon;
 
@@ -9,13 +23,18 @@ import java.io.OutputStream;
 import java.nio.ByteBuffer;
 
 /**
- *
+ * OutputStream wrapper for SIMON RawChannel
  * @author achristian
+ * @since 1.2.0
  */
 public class RawChannelOutputStream extends OutputStream {
     
     private final RawChannel rawChannel;
 
+    /**
+     * To create an outputstream for a rawchannel, you actually need an raw channel
+     * @param rawChannel the raw channel to write to
+     */
     public RawChannelOutputStream(RawChannel rawChannel) {
         this.rawChannel = rawChannel;
     }
