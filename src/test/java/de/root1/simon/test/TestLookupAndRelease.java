09851d6e5dc7fa60c857d5d089f1c290f8a37070
Alex <2862906+tuxedo0801@users.noreply.github.com>
2013-06-19 13:23:40 +0000

    added junit test for multithreaded lookup+release w.r.t. refcounting the session.

--- src/test/java/de/root1/simon/test/TestLookupAndRelease.java
+++ src/test/java/de/root1/simon/test/TestLookupAndRelease.java
@@ -2,13 +2,17 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package de.root1.simon.test;
 
 import de.root1.simon.Lookup;
 import de.root1.simon.NameLookup;
 import de.root1.simon.Registry;
 import de.root1.simon.Simon;
+import de.root1.simon.exceptions.EstablishConnectionFailed;
+import de.root1.simon.exceptions.LookupFailedException;
+import java.net.UnknownHostException;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,7 +24,7 @@ import org.slf4j.LoggerFactory;
  * @author achristian
  */
 public class TestLookupAndRelease {
-    
+
     private final Logger logger = LoggerFactory.getLogger(TestLookupAndRelease.class);
 
     public TestLookupAndRelease() {
@@ -29,11 +33,9 @@ public class TestLookupAndRelease {
     //@BeforeClass
     //public static void setUpClass() throws Exception {
     //}
-
     //@AfterClass
     //public static void tearDownClass() throws Exception {
     //}
-
     @Before
     public void setUp() {
     }
@@ -69,7 +71,7 @@ public class TestLookupAndRelease {
 
             lookup.release(roiRemote);
             logger.info("release of roi done");
-            
+
             logger.info("Awaiting network connections shutdown");
             ((NameLookup) lookup).awaitCompleteShutdown(30000);
             logger.info("Awaiting network connections shutdown *done*");
@@ -92,11 +94,11 @@ public class TestLookupAndRelease {
             RemoteObject roiRemote2 = (RemoteObject) lookup.lookup("roi2");
             roiRemote2.helloWorld();
             lookup2.release(roiRemote2);
-            
+
             logger.info("Awaiting network connections shutdown");
             ((NameLookup) lookup).awaitCompleteShutdown(30000);
             logger.info("Awaiting network connections shutdown *done*");
-            
+
 
             r2.unbind("roi2");
             r2.stop();
@@ -107,24 +109,24 @@ public class TestLookupAndRelease {
             throw new AssertionError(ex);
         }
     }
-    
+
     @Test
     public void LookupAndReleaseMultipleWithoutSleep() {
 
         logger.info("Begin ...");
-        Registry r=null;
+        Registry r = null;
         try {
             RemoteObjectImpl roi = new RemoteObjectImpl();
             r = Simon.createRegistry();
             r.start();
             r.bind("roi", roi);
 
-            for (int i=0;i<20;i++) {
-                
+            for (int i = 0; i < 20; i++) {
+
                 logger.info("\n"
                         + "***********************************\n"
                         + "********* Run:  {}\n"
-                        + "***********************************",i);
+                        + "***********************************", i);
                 try {
 
 
@@ -148,13 +150,13 @@ public class TestLookupAndRelease {
 
                 } catch (Exception ex) {
                     throw new AssertionError(ex);
-                } 
+                }
             }
-            
+
         } catch (Exception e) {
             throw new AssertionError(e);
         } finally {
-            if (r!=null) {
+            if (r != null) {
                 r.unbind("roi");
                 logger.info("unbind of roi done");
                 r.stop();
@@ -163,4 +165,60 @@ public class TestLookupAndRelease {
         }
     }
 
+    @Test
+    public void testMultithreadedLookupAndRelease() throws Exception {
+
+        RemoteObjectImpl roi = new RemoteObjectImpl();
+        Registry r = Simon.createRegistry();
+        r.start();
+        r.bind("server", roi);
+        int count = 3;
+        final Semaphore s = new Semaphore(count);
+        s.acquire(count);
+        Thread[] threads = new Thread[count];
+
+        for (int i = 0; i < threads.length; i++) {
+            final int ii = i+1;
+            threads[i] = new Thread(""+ii) {
+                @Override
+                public void run() {
+                    try {
+                        String name = Thread.currentThread().getName();
+                        logger.info("Thread {} doing lookup", name);
+                        Lookup lookup = Simon.createNameLookup("127.0.0.1");
+                        RemoteObject server = (RemoteObject) lookup.lookup("server");
+                        logger.info("Thread {} got remote object", name);
+                        server.helloWorldArg(Thread.currentThread().getName());
+                        logger.info("Thread {} called method", name);
+                        try {
+                            long sleeptime = ii*10000;
+                            logger.info("Thread {} sleeping for {} ms ...", name, sleeptime);
+                            Thread.sleep(sleeptime);
+                        } catch (InterruptedException ex) {
+                            ex.printStackTrace();
+                        }
+                        logger.info("Thread {} doing release", name);
+                        lookup.release(server);
+                        logger.info("Thread {} ALL DONE", name);
+                        s.release();
+                    } catch (UnknownHostException ex) {
+                        ex.printStackTrace();
+                    } catch (LookupFailedException ex) {
+                        ex.printStackTrace();
+                    } catch (EstablishConnectionFailed ex) {
+                        ex.printStackTrace();
+                    }
+
+                }
+            };
+        }
+        for (int j = 0; j < threads.length; j++) {
+            logger.debug("Starting thread {}", j);
+            threads[j].start();
+        }
+        s.acquire(3);
+        logger.debug("done");
+        r.stop();
+
+    }
 }
