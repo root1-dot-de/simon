d09f3e936aee0d286a14627c9dcb30890e6cf55e
achristian <achristian@8a166049-a23f-4faa-ba0f-b48904010694>
2013-08-23 13:34:34 +0000

    wrong tag

--- tags/1.1.4/1.1.x/src/test/java/de/root1/simon/test/remoteobjectvalidation/IServer.java
+++
@@ -1,6 +0,0 @@
-package de.root1.simon.test.remoteobjectvalidation;
-
-public interface IServer {
-
-    public void test(IClient client);
-}
--- tags/1.1.4/1.1.x/src/test/java/de/root1/simon/test/utils/BasisClass.java
+++
@@ -1,8 +0,0 @@
-package de.root1.simon.test.utils;
-
-import de.root1.simon.annotation.SimonRemote;
-
-@SimonRemote(value={BasisInterface.class})
-public class BasisClass implements ExtendedBasisInterface {
-	private static final long serialVersionUID = 7059327535633065297L;
-}
