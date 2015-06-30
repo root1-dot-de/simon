3b5192e4cc06f8e9c1a53968da0a1cb33d556706
achristian <achristian@8a166049-a23f-4faa-ba0f-b48904010694>
2015-06-30 09:54:31 +0000

    fixed issue #193. bumped version to 1.2.1

--- branches/1.2.x/src/main/java/de/root1/simon/filetransmit/FileSenderProgressListener.java
+++ branches/1.2.x/src/main/java/de/root1/simon/filetransmit/FileSenderProgressListener.java
@@ -7,14 +7,41 @@ package de.root1.simon.filetransmit;
 import java.io.File;
 
 /**
- *
+ * Progress Listener interface for file transfer
  * @author achristian
  */
 public interface FileSenderProgressListener {
 
+    /**
+     * File-Transfer has been started
+     * @param id the ID for that transfer. See also {@link DefaultFileSender#sendFile(java.io.File) }
+     * @param f the related file
+     * @param length the length in bytes of datan to transfer
+     */
     public void started(int id, File f, long length);
+    
+    /**
+     * 
+     * @param id the ID for that transfer. See also {@link DefaultFileSender#sendFile(java.io.File) }
+     * @param f the related file
+     * @param bytesSent
+     * @param length 
+     */
     public void inProgress(int id, File f, long bytesSent, long length);
+    
+    /**
+     * 
+     * @param id the ID for that transfer. See also {@link DefaultFileSender#sendFile(java.io.File) }
+     * @param f the related file
+     */
     public void completed(int id, File f);
+    
+    /**
+     * 
+     * @param id the ID for that transfer. See also {@link DefaultFileSender#sendFile(java.io.File) }
+     * @param f the related file
+     * @param ex 
+     */
     public void aborted(int id, File f, Exception ex);
     
 }
